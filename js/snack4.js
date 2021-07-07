2c995ffb1a55ea34524ac4e6a78993b4ae4f62a0
Roccofranchini <rocco.franchini1@gmail.com>
2021-07-07 17:20:32 +0200

    snack4

---
+++ js/snack4.js
@@ -0,0 +1,42 @@
+/*
+SNACK 4: Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro
+
+1- creiamo i due array con n elementi
+2- attraverso un ciclo while generiamo un numero casuale fino a quando le "lunghezze" degli array non si equivalgono
+3- tramite un if pushiamo il numero generato ad ogni giro alla lista più corta
+4- stampiamo
+
+*/
+
+// creiamo gli array
+
+var nList1 = ['1', '2', '3', '4', '8', '10'];
+var nList2 = ['1', '2', '3'];
+
+// pushiamo attraverso il while i numeri nella lista più corta
+
+while (nList1.length !== nList2.length) {
+
+    var nRandom = Math.floor(Math.random() * 100);
+
+    console.log(nRandom);
+
+    if (nList1.length > nList2.length) {
+        nList2.push(nRandom);
+    }
+
+    else {
+        nList1.push(nRandom);
+    }
+
+}
+
+// stampiamo
+
+console.log(nList1);
+console.log(nList2);
+
+
+
